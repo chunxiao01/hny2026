07cd452eef6a157233a7872a1d26ff7b41617a09
chunxiao01 <12861187+chunxiao01@users.noreply.github.com>
2026-02-15 19:19:20 +0800

    deploy

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/hny2026/sw.js', { scope: '/hny2026/' })})}
