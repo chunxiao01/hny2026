0fe223c42c911594e3c3f2e82af953972e3499c6
chunxiao01 <12861187+chunxiao01@users.noreply.github.com>
2026-02-15 19:43:47 +0800

    the first commit.

--- vite.config.ts
+++ vite.config.ts
@@ -30,17 +30,17 @@ export default defineConfig({
         orientation: 'portrait',
         icons: [
           {
-            src: '/hny2026/assets/images/horse180.png',
+            src: '/images/horse180.png',
             sizes: '180x180',
             type: 'image/png',
           },
           {
-            src: '/hny2026/assets/images/horse192.png',
+            src: '/images/horse192.png',
             sizes: '192x192',
             type: 'image/png',
           },
           {
-            src: '/hny2026/assets/images/horse512.png',
+            src: '/images/horse512.png',
             sizes: '512x512',
             type: 'image/png',
           },
